3f7e9fafff9b2053eb968c879f3d806ef716536a
danny539 <danny539@csu.fullerton.edu>
2019-09-14 15:26:24 -0700

    prob03-danny539

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -10,13 +10,15 @@ int main()
   double height2;
   double height3;
 
-  std::cout<< "Enter the person's height in inches" << std::endl;
+  std::cout << "Enter the person's height in inches" << std::endl;
   std::cin >> height;
 
-  height2= height / 12;
-  height3= height % 12;
+  height2 = height / 12; //the interger divison makes it so that we get an
+  //integer as the answer
+  height3 = height % 12; //and with this we are able to know that the outcome of
+  // this will result in feet and inches as the answer
 
-  std::cout<<" That person is " << height2 <<"\'" << height3 << "\""<< std::endl;
+  std::cout << " That person is " << height2 << "\'" << height3 << "\"" << std::endl;
 
   return 0;
 
